89fbabb18959399a4194d93f40892c133893ac0c
Ziad <zslah2800@gmail.com>
2025-04-28 18:24:01 +0300

    init main

---
+++ lib/complete_store_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompleteStoreApp extends StatelessWidget {
+  const CompleteStoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'E-Store App');
+  }
+}
