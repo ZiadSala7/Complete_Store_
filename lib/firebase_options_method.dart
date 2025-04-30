223be031a36886ab230ed41dd2b503b10002a193
Ziad <zslah2800@gmail.com>
2025-04-30 14:05:43 +0300

    adding firebase to the project

---
+++ lib/firebase_options_method.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseOptions firebaseOptionsMethod() {
+  return FirebaseOptions(
+    apiKey: 'AIzaSyAOvnjA3bQMshhVHBDtpuE0BG_JBHQiTaI',
+    appId: '1:491579379138:android:07aaeb54548c8a55845a5b',
+    messagingSenderId: '491579379138',
+    projectId: 'asroo-store-31843',
+  );
+}
+// get it from ==> "google_services.json"
+/*
+  "current_key": "AIzaSyAOvnjA3bQMshhVHBDtpuE0BG_JBHQiTaI",
+  "mobilesdk_app_id": "1:491579379138:android:07aaeb54548c8a55845a5b",
+  "project_number": "491579379138",
+  "project_id": "asroo-store-31843",
+*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'complete_store_app.dart';
+import 'firebase_options_method.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(options: firebaseOptionsMethod())
+      : await Firebase.initializeApp();
   runApp(const CompleteStoreApp());
 }
